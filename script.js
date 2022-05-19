4763432e3b98fc312c21cd2328999e7a459206e1
unknown <viniciusdeab@outlook.com>
2022-05-19 11:34:40 -0300

    Identificando as combinações para obter uma vitoria

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ const board = document.querySelector("[data-board]");
 
 let isCircleTurn;
 
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
 const startGame = () => {
     for (const cell of cellElements) {
         cell.addEventListener("click", handleClick, {once: true});
@@ -11,6 +22,14 @@ const startGame = () => {
     isCircleTurn = false;
 
     board.classList.add("x");
+};
+
+const checkForWin = (currentPlayer) => {
+    return winningCombinations.some((combination) =>{
+        return combination.every((index) =>{
+            return cellElements[index].classList.contains(currentPlayer);
+        });
+    });
 };
 
 const placeMark = (cell, classToAdd) =>{
